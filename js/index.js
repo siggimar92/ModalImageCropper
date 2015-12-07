5ff06739a2c7a8b60fcc9f822839845381861942
Sigurður Atlason <sigurdura13@ru.is>
2015-12-07 21:46:22 +0000

    changes to make more user friendly

--- js/index.js
+++ js/index.js
@@ -15,9 +15,9 @@ var upload = document.getElementsByTagName('input')[0],
     originalHeight = 0,
     scaledWidth = 0,
     scaledHeight = 0,
-    canvasWidth = 550;
+    //canvasWidth = 550,
     cropSize = 200,
-    cropOffsetX = (canvasWidth / 2) - (cropSize / 2),
+    cropOffsetX = 0,
     cropOffsetY = 0;
 
 upload.onchange = function (e) {
@@ -33,23 +33,28 @@ upload.onchange = function (e) {
     reader = new FileReader()
     reader.onload = function (event) {
     	// TODO: reset values in more nice way, scaler in HTML does not update when new image is selected.
-    	img = new Image(), scale = 1, lastX = 0, lastY = 0, isDown = false, baseX = 0, baseY = 0, originalWidth = 0, originalHeight = 0;
-    	$('#scaleSlider').value = 1;
+    	initiateVariables();
 
       img.onload = function () {
-        context.canvas.width = canvasWidth;
         // set image size so it will fit in canvas
         originalWidth = img.width;
         originalHeight = img.height;
-        img.width = canvas.width;
-        img.height = (originalHeight / originalWidth) * img.width;
-        cropOffsetY = (img.height / 2) - (cropSize / 2);
-
-        if (img.height < 300) {
-          context.canvas.height = 300;
-        } else {
+        // if img is widescreen then height is = cropsize
+        if (img.width > img.height) {
+          img.height = cropSize;
+          img.width = (originalWidth / originalHeight) * img.height;
+          context.canvas.width = img.width;
+          context.canvas.height = img.height;
+        } else { // if img is not widescreen then width is = cropsize
+          img.width = cropSize;
+          img.height = (originalHeight / originalWidth) * img.width;
+          context.canvas.width = img.width;
           context.canvas.height = img.height;
         }
+        
+        cropOffsetX = (img.width / 2) - (cropSize / 2);
+        cropOffsetY = (img.height / 2) - (cropSize / 2);
+
 
         drawImage(0, 0);
       }
@@ -77,25 +82,16 @@ upload.onchange = function (e) {
   	$('#beforeCrop').hide();
   	$('#afterCrop').show();
   	
-  	var tmpContext, tmpCanvas, src;
+  	var tmpContext, tmpCanvas, imgSrc;
   	tmpCanvas = document.createElement('canvas');
   	tmpContext = tmpCanvas.getContext('2d');
   	tmpCanvas.width = cropSize;
   	tmpCanvas.height = cropSize;
   	tmpContext.drawImage(context.canvas, cropOffsetX, cropOffsetY, cropSize, cropSize, 0, 0, cropSize, cropSize);
-  	src = tmpCanvas.toDataURL();
-  	document.getElementById('croppedImg').src = src;
-  };
-
-  document.getElementById('reset').onclick = function (e) {
-  	e.preventDefault();
-  	scale = 1, lastX = 0, lastY = 0, isDown = false, baseX = 0, baseY = 0, originalWidth = 0, originalHeight = 0;
-  	$('#scaleSlider').value = 1;
-  	originalWidth = img.width;
-  	originalHeight = img.height;
-  	img.width = canvas.width;
-  	img.height = (originalHeight / originalWidth) * img.width;
-  	drawImage(0, 0);
+  	// Base64 url png by default
+    imgSrc = tmpCanvas.toDataURL();
+    console.log("base64: ", imgSrc);
+  	document.getElementById('croppedImg').src = imgSrc;
   };
 
   document.getElementById('save').onclick = function (e) {
@@ -111,7 +107,21 @@ upload.onchange = function (e) {
   };
 
   function initiateVariables() {
-
+    img = new Image();
+    scale = 1;
+    isDown = false;
+    baseX = 0;
+    baseY = 0;
+    lastX = 0;
+    lastY = 0;
+    originalWidth = 0;
+    originalHeight = 0;
+    scaledWidth = 0;
+    scaledHeight = 0;
+    cropSize = 200;
+    cropOffsetX = 0;
+    cropOffsetY = 0;
+    $('#scaleSlider').value = 1;
   };
 
   /* Redraws image on canvas after any change */
@@ -124,16 +134,16 @@ upload.onchange = function (e) {
     lastX = x;
     lastY = y;
 
-    scaledWidth = Math.floor(img.width * scale);
-    scaledHeight = Math.floor(img.height * scale);
+    scaledWidth = img.width * scale;
+    scaledHeight = img.height * scale;
     // draw the image on the canvas
-    context.drawImage(img, baseX, baseY, Math.floor(img.width * scale), Math.floor(img.height * scale));
+    context.drawImage(img, baseX, baseY, img.width * scale, img.height * scale);
     // the alpha rect over image to show cropped area
     drawCropOverlay();
   };
 
   function drawCropOverlay() {
-    context.fillStyle = 'rgba(242, 242, 242, 0.8)';
+    context.fillStyle = 'rgba(255, 255, 255, 0.8)';
     context.beginPath();
     context.rect(0, 0, canvas.width, canvas.height);
     context.moveTo(cropOffsetX, cropOffsetY);
@@ -160,10 +170,13 @@ upload.onchange = function (e) {
     isDown = true;
     lastX = coord.x;
     lastY = coord.y;
-    // console.log("X: ", lastX);
-    // console.log("Y: ", lastY);
-    // console.log("base x: ", baseX);
-    // console.log("base y: ", baseY);
+
+    // console.log("lastX: ", lastX);
+    // console.log("lastY: ", lastY);
+    // console.log("baseX: ", baseX);
+    // console.log("baseY: ", baseY);
+    // console.log("cropOffsetX: ", cropOffsetX);
+    // console.log("cropOffsetY: ", cropOffsetY);
     // console.log("cropOffsetX + cropSize: ", cropOffsetX + cropSize);
     // console.log("cropOffsetY + cropSize: ", cropOffsetY + cropSize);
 
@@ -172,26 +185,33 @@ upload.onchange = function (e) {
   /* On mouse move */
   canvas.onmousemove = function(e) {
   	e.preventDefault();
+
+    var imgLeft = baseX, 
+        imgTop = baseY, 
+        imgRight = (baseX + scaledWidth), 
+        imgBottom = (baseY + scaledHeight), 
+        cropLeft = cropOffsetX, 
+        cropTop = cropOffsetY, 
+        cropRight = (cropOffsetX + cropSize), 
+        cropBottom = (cropOffsetY + cropSize);
+
   	if (isDown) {
   		var coord = getMouseCoord(canvas, e);
-  		drawImage(coord.x, coord.y);
-      if ((baseX >= cropOffsetX) || (baseY >= cropOffsetY) || ((baseX + scaledWidth) <= (cropOffsetX + cropSize)) || (baseY + scaledHeight) <= (cropOffsetY + cropSize)) {
-        // TODO: image should not get past this point fix it
-        console.log("DO STUFF!");
-      }
-  	}
-
-      // if (isDown) {
-      //   var coord = getMouseCoord(canvas, e);
-        
-      //   if ((baseX >= cropOffsetX) || (baseY >= cropOffsetY) || ((baseX + scaledWidth) <= (cropOffsetX + cropSize)) || (baseY + scaledHeight) <= (cropOffsetY + cropSize)) {
-      //     // TODO: image should not get past this point fix it
-      //     console.log("DO STUFF!");
-      //   } else {
-      //     drawImage(coord.x, coord.y);
-      //   }
-      // }
       
+      if (imgLeft >= cropLeft) { /* checks left side */
+        baseX = cropLeft;
+      }
+      if (imgTop >= cropTop) { /* checks top */
+        baseY = cropTop;
+      }
+      if (imgRight <= cropRight) { /* checks right side */
+        baseX = cropOffsetX - scaledWidth + cropSize;
+      }
+      if (imgBottom <= cropBottom) { /* checks bottom */
+        baseY = cropOffsetY - scaledHeight + cropSize;
+      }
+      drawImage(coord.x, coord.y);      
+  	}      
   };
 
   /* On mouse up */
@@ -204,19 +224,12 @@ upload.onchange = function (e) {
 };
 
 
+// if ((baseX >= cropOffsetX) || 
+//    (baseY >= cropOffsetY) || 
+//    ((baseX + scaledWidth) <= (cropOffsetX + cropSize)) || 
+//    (baseY + scaledHeight) <= (cropOffsetY + cropSize)) {
 
-
-
-
-// var originalWidth = img.width;
-// var originalHeight = img.height;
-// var isWider = false;
-// if (originalWidth > canvas.width) {
-//   img.width = canvas.width;
-//   img.height = (originalHeight / originalWidth) * img.width;
-//   isWider = true;
-// }
-// if (originalHeight > canvas.height && !isWider) {
-//   img.height = canvas.height;
-//   img.width = (originalWidth / originalHeight) * img.height;
+//   // TODO: image should not get past this point fix it
+//   console.log("DO STUFF!");
+//   //isDown = false;
 // }
